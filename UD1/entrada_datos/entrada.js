f85df1cc478e14f7bdcc0ed8ccc5f052c1940d4f
Andrea Elizabeth Bushnell Karlsson <andreabushnell00@gmail.com>
2025-09-17 09:55:18 +0200

    UD2-16/09

---
+++ UD1/entrada_datos/entrada.js
@@ -0,0 +1,5 @@
+function ejecuta() {
+    let dia = document.getElementById("dia").value;
+    let mes = document.getElementById("mes").value;
+    document.getElementById("resultado").innerHTML= `El dia es: ${dia} y el mes es: ${mes}`;
+}
