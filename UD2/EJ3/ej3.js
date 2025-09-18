ee1a0bb9654980c724ffac3a5d60d5aa6136630a
Andrea Elizabeth Bushnell Karlsson <andreabushnell00@gmail.com>
2025-09-18 14:04:29 +0200

    UD1-fix

--- UD2/EJ3/ej3.js
+++
@@ -1,9 +0,0 @@
-let op1 = (10==10);
-let op2 =  (10===10);
-let op3 = (10===10.0);
-let op4 = ("Laura" == "laura");
-let op5 = ("Laura" > "laura");
-let op6 = ("Laura" < "laura");
-let op7 = ("123"==123);
-let op8 = ("123"==123);
-let op9 = (parseInt("123")===123);
