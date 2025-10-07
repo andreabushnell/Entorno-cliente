3b1bbc17f2cc40f903a1e908b1bea47f6088c554
Andrea Elizabeth Bushnell Karlsson <andreabushnell00@gmail.com>
2025-10-07 09:59:59 +0200

    UD3-07/10

--- UD3/EJ13/ej13.js
+++ UD3/EJ13/ej13.js
@@ -4,23 +4,20 @@ function crear(){
     ventana = window.open("","","width=500,height=200");
     ventana.outerHeight=200;
     ventana.outerWidth=500;
-    ventana.document.write("<h1>Mi ventana</h1>");
 
-    let ventana_origen = ventana.opener;
     let intervaloIda = setInterval(()=>{
         ventana.moveBy(10,10);
-        if(ventana.screenX==ventana_origen.outerWidth){
-            ventana.moveBy(-10,-10);
-        } else if (ventana.screenX==0) {
+        if (ventana.screenY==(window.innerHeight-ventana.outerHeight+5)) {
             clearInterval(intervaloIda);
         }
-    },100);
+    },10);
+
     let intervaloVuelta = setInterval(()=>{
         ventana.moveBy(-10,-10);
-        if (ventana.screenX==0 && ventana.screenY==0) {
+        if (ventana.screenX==0) {
             clearInterval(intervaloVuelta);
         }
-    },100);
+    },10);
 }
 
 function parar(){
--- UD3/EJ14/ej14.js
+++ UD3/EJ14/ej14.js
@@ -1,26 +1,35 @@
 var pc_nom; var letra; var num;
-const regex = /[A-Z\d]*(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[A-C]{3}/;
+
+const regex = /[A-Z\d]*(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[A-C]{1}/;
 
 function mostrarIP() {
     pc_nom = (document.getElementById("pc-nom").value).toUpperCase();
-    if (pc_nom.matches(regex)[0]==pc_nom){
-        letra = pc_nom[pc_nom.length];
-        num = pc_nom.slice[pc_nom.length-4, pc_nom.length-1];
+
+    let encontrado = pc_nom.match(regex);
+    let longitud = pc_nom.length;
+    
+    if (encontrado[0]==pc_nom){
+
+        letra = pc_nom[longitud-1];
+       
+        num = pc_nom.substring(longitud-4, longitud-1);
+
+        resultado = document.getElementById("resultado").innerHTML;
         switch(letra){
             case 'A': {
-                document.getElementById("resultado").innerHTML = "10.42.68."+num;
+                resultado = "IP: 10.42.68."+num;
             }
             break;
             case 'B': {
-                document.getElementById("resultado").innerHTML = "10.42.69."+;
+                resultado = "IP: 10.42.69."+num;
             }
             break;
             case 'C': {
-                document.getElementById("resultado").innerHTML = "10.52.178."+num;
+                resultado = "IP: 10.52.178."+num;
             }
             break;
         }
     } else {
-        document.getElementById("resultado").innerHTML = "Formato erróneo"+num;
+        resultado = "Formato erróneo";
     }
 }
