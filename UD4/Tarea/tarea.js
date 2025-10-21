ee50853178c6bfe2413a4e14db0e76bf3faddad3
Andrea Elizabeth Bushnell Karlsson <andreabushnell00@gmail.com>
2025-10-21 10:59:36 +0200

    UD4-21/10-1

---
+++ UD4/Tarea/tarea.js
@@ -0,0 +1,38 @@
+class Edificio {
+    constructor (calle, numero, cod_post) {
+        this.calle = calle;
+        this.numero = numero;
+        this.cod_post = cod_post;
+        this.plantas = [];
+    }
+
+    agregarPlantasYPuertas(numPlantas, puertas){
+
+    }
+    modificarNumero(numero){
+
+
+    }
+    modificarCalle(calle){
+
+    }
+    modificarCodigoPostal(codigo){
+
+    }
+    imprimeCalle(){
+
+    }
+    imprimeNumero (){
+
+    }
+    imprimeCodigoPostal (){
+
+    }
+    agregarPropietario(nombre, planta , puerta){
+
+    }
+    imprimePlantas (){
+        
+    }
+
+}
