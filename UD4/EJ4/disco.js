1a8f40d27a6cb550b0ed92ffa78ebeb5f66789d1
Andrea Elizabeth Bushnell Karlsson <andreabushnell00@gmail.com>
2025-10-22 11:32:12 +0200

    UD4-22/10-1

--- UD4/EJ4/disco.js
+++ UD4/EJ4/disco.js
@@ -5,8 +5,7 @@ class Disco {
         nombre,
         autor,
         año_publicacion,
-        genero,
-        caratula
+        genero
     ) {
         this.nombre = nombre;
         this.autor = autor;
@@ -14,16 +13,16 @@ class Disco {
         this.genero = genero;
         this.ubicacion = 0;
         this.prestado = false;
-        this.caratula = caratula;
+        this.caratula = "imagen.png";
     }
 
-   /* set ubicacion (ubicacion) {
+    set ubicacion (ubicacion) {
         this.ubicacion = ubicacion;
-    }*/
+    }
 
-   /* set prestado (prestado){
+    set prestado (prestado){
         this.prestado = prestado;
-    }*/
+    }
 
     mostrarInfoDisco() {
         return `    
@@ -41,180 +40,35 @@ class Disco {
     }
 }
 
+let listado_discos = [];
 
+function mostrarNumDiscos () {
 
-let formulario = document.getElementById("formulario");
+}
+function mostrarTablaDiscso() {
 
-formulario.addEventListener("change", function () {
+}
+function mostrarIntervalodDiscos(incio, fin) {
 
-    let listado_discos = [
-        new Disco(
-            "Thriller",
-            "Michael Jackson",
-            1982,
-            "Pop",
-            "thriller-cover.png"
-        ),
-        new Disco(
-            "Back in Black",
-            "AC/DC",
-            1980,
-            "Rock",
-            "back_in_black-cover.jpg"
-        ),
-        new Disco(
-            "The Bodyguard",
-            "Whitney Houston",
-            1992,
-            "Pop",
-            "the_bodyguard-cover.png"
-        ),
-    ];
+}
+function añadirDisco() {
+    if(selection = principio) {
 
-    let opciones_menu = document.getElementsByName("opciones-menu");
-    opciones_menu.forEach((opcion) => {
-        if (opcion.checked) {
-            document.getElementsByClassName(opcion.value)[0].style.display = "block";
+    } else {
+
+    }
+}
+function borrarDisco() {
+    if(selection = principio) {
+
+    } else {
         
-            switch (opcion.value) {
+    }
+}
+function consultarDisco() {
+    if(selection = posicion) {
 
-                case "mostrar-cantidad": {
-                    if (opcion.checked) {
-                        document.getElementById("cantidad-discos").innerHTML = `Hay ${arrays.mostrarTamano(listado_discos)} discos`;
-                    }
-                } break;
-
-                case "mostrar-tabla": {
-
-                    let opciones_tabla = document.getElementsByName("opciones-orden-tabla");
-
-                    opciones_tabla.forEach((opcion) => {
-                        if (opcion.checked) {
-                        document.getElementsByClassName(opcion.value)[0].style.display = "block";
-                            let tabla = document.getElementsByClassName(opcion.value)[0];
-                            switch (opcion.value) {
-                                case "orden-normal": {
-                                    arrays.mostrarListado(listado_discos).forEach((disco) => {
-                                        tabla.insertRow().insertCell().innerHTML = disco.mostrarInfoDisco();
-                                    });
-                                } break;
-                                case "orden-inverso": {
-                                    arrays.mostrarListadoInverso(listado_discos).forEach((disco) => {
-                                        tabla.insertRow().insertCell().innerHTML = disco.mostrarInfoDisco();
-                                    });
-                                } break;
-                                case "orden-alfabeto": {
-                                    arrays.mostrarListadoAlfabetico(listado_discos).forEach((disco) => {
-                                        tabla.insertRow().insertCell().innerHTML = disco.mostrarInfoDisco();
-                                    });
-                                } break;
-                            }
-                        } else {
-                            document.getElementsByClassName(opcion.value)[0].style.display = "none";
-                        }
-                    });
-                } break;
-
-                case "mostrar-intervalo":{
-                    let min = document.getElementById("min").value;
-                    let max = document.getElementById("max").value;
-                    document.getElementById("mostrar-intervalo").addEventListener("click", function() {
-                        listado_discos.forEach(disco => {
-                        if(disco.año_publicacion >= min && disco.año_publicacion <= max)
-                            document.getElementById("intervalo").innerHTML += disco.mostrarInfoDisco();
-                        });
-                    });
-                    
-                } break;
-
-                case "añadir-disco": {
-                    let opciones_añadir = document.getElementsByName("opciones-añadir");
-
-                    opciones_añadir.forEach((opcion) => {
-                        if (opcion.checked) {
-                            
-                            switch (opcion.value) {
-                                case "añadir-principio": {
-                                    document.getElementById("añadir").addEventListener("click", function () {
-                                        let disco_a_añadir = new Disco(
-                                        document.getElementById("nombre-disco-a-añadir").value,
-                                        document.getElementById("autor-disco-a-añadir").value,
-                                        document.getElementById("año-disco-a-añadir").value,
-                                        document.getElementById("genero-disco-a-añadir").value,
-                                        document.getElementById("caratula-disco-a-añadir").value
-                                        );
-                                        arrays.añadirPrincipio(listado_discos, disco_a_añadir);
-                                    });
-                                } break;
-                                case "añadir-final": {
-                                    document.getElementById("añadir").addEventListener("click", function () {
-                                        let disco_a_añadir = new Disco(
-                                        document.getElementById("nombre-disco-a-añadir").value,
-                                        document.getElementById("autor-disco-a-añadir").value,
-                                        document.getElementById("año-disco-a-añadir").value,
-                                        document.getElementById("genero-disco-a-añadir").value,
-                                        document.getElementById("caratula-disco-a-añadir").value
-                                        );
-                                        arrays.añadirFinal(listado_discos, disco_a_añadir);
-                                    });
-                                } break;
-                            }
-                        } 
-                    });
-                } break;
-
-                case "eliminar-disco": {
-                    let opciones_eliminar = document.getElementsByName("opciones-eliminar");
-
-                    opciones_eliminar.forEach((opcion) => {
-                        if (opcion.checked) {
-                            
-                            switch (opcion.value) {
-                                case "eliminar-principio": {
-                                    document.getElementById("eliminar").addEventListener("click", function () {
-                                        arrays.eliminarPrincipio(listado_discos);
-                                    });
-                                } break;
-                                case "eliminar-final": {
-                                    document.getElementById("eliminar").addEventListener("click", function () {
-                                        arrays.eliminarFinal(listado_discos);
-                                    });
-                                } break;
-                            }
-                        } 
-                    });
-                } break;
-                case "consultar-disco": {
-
-                    let opciones_consulta = document.getElementsByName("consulta");
-
-                    opciones_consulta.forEach((opcion) => {
-                        if (opcion.checked) {
-                            document.getElementsByClassName(opcion.value)[0].style.display = "block";
-                            let id_consulta = document.getElementsByClassName(opcion.value)[0].children.item(1).id;
-                            switch (id_consulta) {
-                                case "nombre-disco": {
-                                    document.getElementById("consultar-nombre").addEventListener("click", function () {
-                                        let nombre = document.getElementById("nombre-disco").value;
-                                        document.getElementById("resultado-consulta-nombre").innerHTML += (arrays.mostrarPorNombre(listado_discos, nombre));
-                                    });
-                                } break;
-                                case "posicion-disco": {
-                                    document.getElementById("consultar-posicion").addEventListener("click", function () {
-                                        let posicion = parseInt(document.getElementById("posicion-disco").value);
-                                        document.getElementById("resultado-consulta-posicion").innerHTML += arrays.mostrarPorPosicion(listado_discos, posicion);
-                                    });
-                                } break;
-                            }
-                        } else {
-                            document.getElementsByClassName(opcion.value)[0].style.display = "none";
-                        }
-                    });
-                } break;
-            }
-        } else {
-            document.getElementsByClassName(opcion.value)[0].style.display = "none";
-        }
-    });
-});
-
+    } else {
+        
+    }
+}
